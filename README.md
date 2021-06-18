0737a9d296621ff89768f854c82dba64097d9c51
GoodnewsIyeteku <gd.iyeteku@yahoo.ca>
2021-06-18 12:17:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# COMP229-M2021-Week6c
+
+Assignment 2
---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost/clothing_store";
+export const RemoteURI = "mongodb+srv://admin:<password1234>@cluster-comp229.ykyhx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const HostName = "RemoteHost";
+export const Secret = "someSecret";
